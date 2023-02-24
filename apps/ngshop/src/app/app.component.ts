5d7166a5fa959ec7f91f2e9ef8ad5f602aea74e8
soulaichhi <chhibou.soulaimane@gmail.com>
2023-02-24 19:06:41 +0100

    Initial commit

---
+++ apps/ngshop/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ang-apps-monorepo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ngshop';
+}
