177956cc072d02298e06529cf9059c24b7ef6202
soulaichhi <chhibou.soulaimane@gmail.com>
2023-03-02 23:46:30 +0100

    Add ColorPicker for Category Color

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -21,7 +21,7 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ToastModule } from 'primeng/toast';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
-
+import { ColorPickerModule } from 'primeng/colorpicker';
 const UX_MODULES = [
   CardModule,
   ToolbarModule,
@@ -30,6 +30,7 @@ const UX_MODULES = [
   InputTextModule,
   ToastModule,
   ConfirmDialogModule,
+  ColorPickerModule,
 ];
 @NgModule({
   declarations: [
