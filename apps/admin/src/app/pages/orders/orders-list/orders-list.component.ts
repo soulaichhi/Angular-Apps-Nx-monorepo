8f6c64f549d8ba9d23accf7511ba16b12fb0ceb2
soulaichhi <chhibou.soulaimane@gmail.com>
2023-03-08 13:51:41 +0100

    format the Order Status

--- apps/admin/src/app/pages/orders/orders-list/orders-list.component.ts
+++ apps/admin/src/app/pages/orders/orders-list/orders-list.component.ts
@@ -1,6 +1,28 @@
 import { Order, OrdersService } from '@ang-apps-monorepo/orders';
 import { Component, OnInit } from '@angular/core';
 
+const ORDER_STATUS = {
+  0: {
+    label: 'Pending',
+    color: 'primary',
+  },
+  1: {
+    label: 'Processed',
+    color: 'warning',
+  },
+  2: {
+    label: 'Shipped',
+    color: 'warning',
+  },
+  3: {
+    label: 'Delivered',
+    color: 'success',
+  },
+  4: {
+    label: 'Failed',
+    color: 'danger',
+  },
+};
 @Component({
   selector: 'admin-orders-list',
   templateUrl: './orders-list.component.html',
@@ -8,6 +30,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersListComponent implements OnInit {
   orders: Order[] = [];
+  orderStatus = ORDER_STATUS;
   constructor(private ordersService: OrdersService) {}
   ngOnInit(): void {
     this._getOrders();
