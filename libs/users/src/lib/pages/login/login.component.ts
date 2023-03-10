02a8c906c5d60d6fab48cce70cbcd7aab3f0d643
soulaichhi <chhibou.soulaimane@gmail.com>
2023-03-10 15:46:57 +0100

    Login Service & Retrieve token

--- libs/users/src/lib/pages/login/login.component.ts
+++ libs/users/src/lib/pages/login/login.component.ts
@@ -1,5 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'users-login',
   templateUrl: './login.component.html',
@@ -8,7 +10,9 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginFormGroup: FormGroup;
   isSubmitted = false;
-  constructor(private formBuilder: FormBuilder) {}
+  authError = false;
+  authMessage = 'Email or Password are wrong';
+  constructor(private formBuilder: FormBuilder, private auth: AuthService) {}
   ngOnInit(): void {
     this._initFormGroup();
   }
@@ -20,6 +24,23 @@ export class LoginComponent implements OnInit {
   }
   onSubmit() {
     this.isSubmitted = true;
+    if (this.loginFormGroup.invalid) return;
+    const loginData = {
+      email: this.loginForm.email.value,
+      password: this.loginForm.password.value,
+    };
+    this.auth.login(loginData.email, loginData.password).subscribe(
+      (user) => {
+        this.authError = false;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+        this.authError = true;
+        if (error.status !== 400) {
+          this.authMessage = 'Error in the server, try again later !';
+        }
+      }
+    );
   }
   get loginForm() {
     return this.loginFormGroup.controls;
