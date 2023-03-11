632281d90cc18dbf4466f18a9dbdc613955b786a
soulaichhi <chhibou.soulaimane@gmail.com>
2023-03-11 15:43:16 +0100

    Dashboard Styling

--- apps/admin/src/app/pages/dashboard/dashboard.component.ts
+++ apps/admin/src/app/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'admin-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {}
