f9e2a50d012e90fad5e96d25cca394a5956595aa
soulaichhi <chhibou.soulaimane@gmail.com>
2023-03-11 12:05:12 +0100

    Read Token Data - isAdmin & Expiration

--- libs/users/src/lib/services/auth-guard.service.ts
+++ libs/users/src/lib/services/auth-guard.service.ts
@@ -18,9 +18,16 @@ export class AuthGuardService implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const token = this.localStorageToken.getToken();
     if (token) {
-      return true;
+      const tokenDecode = JSON.parse(atob(token.split('.')[1]));
+      console.log(tokenDecode);
+      if (tokenDecode.isAdmin && !this._tokenExpired(tokenDecode)) {
+        return true;
+      }
     }
     this.router.navigate(['/login']);
     return false;
+  }
+  private _tokenExpired(expiration): boolean {
+    return Math.floor(new Date().getTime() / 1000) >= expiration;
   }
 }
