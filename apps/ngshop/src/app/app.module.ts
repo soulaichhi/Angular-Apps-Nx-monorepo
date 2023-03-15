43d37754e90bfe1fb14a3dbec97c22dcee802ae2
soulaichhi <chhibou.soulaimane@gmail.com>
2023-03-15 22:55:18 +0100

    Create the product Search Component

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
@@ -8,9 +8,10 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { UiModule} from '@ang-apps-monorepo/ui';
-import { AccordionModule} from 'primeng/accordion';
+import { UiModule } from '@ang-apps-monorepo/ui';
+import { AccordionModule } from 'primeng/accordion';
 import { NavComponent } from './shared/nav/nav.component';
+import { ProductsModule } from '@ang-apps-monorepo/products';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +26,8 @@ import { NavComponent } from './shared/nav/nav.component';
     BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes),
     UiModule,
-    AccordionModule
+    AccordionModule,
+    ProductsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
