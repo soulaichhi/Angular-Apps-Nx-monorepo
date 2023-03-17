aefab071a5202c5ba77821ada0f1497d1cdf98d3
soulaichhi <chhibou.soulaimane@gmail.com>
2023-03-17 11:15:22 +0100

    Create Products page

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ProductListComponent } from './pages/product-list/product-list.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { AccordionModule } from 'primeng/accordion';
@@ -17,7 +16,6 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     HomePageComponent,
-    ProductListComponent,
     HeaderComponent,
     FooterComponent,
     NavComponent,
---
+++ libs/products/src/lib/pages/products-list/products-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { Category } from '../../models/category';
+import { Product } from '../../models/product';
+import { CategoriesService } from '../../services/categories.service';
+import { ProductsService } from '../../services/products.service';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'products-list',
+  templateUrl: './products-list.component.html',
+  styles: [],
+})
+export class ProductsListComponent implements OnInit, OnDestroy {
+  products: Product[] = [];
+  categories: Category[] = [];
+  endsubs$: Subject<any> = new Subject();
+  constructor(
+    private productsService: ProductsService,
+    private categoriesService: CategoriesService
+  ) {}
+  ngOnInit(): void {
+    this._getProducts();
+    this._getCategories();
+  }
+  ngOnDestroy(): void {
+    this.endsubs$.next(true);
+    this.endsubs$.complete();
+  }
+  private _getProducts() {
+    this.productsService
+      .getProducts()
+      .pipe(takeUntil(this.endsubs$))
+      .subscribe((products) => {
+        this.products = products;
+      });
+  }
+  private _getCategories() {
+    this.categoriesService
+      .getCategories()
+      .pipe(takeUntil(this.endsubs$))
+      .subscribe((categories) => {
+        this.categories = categories;
+      });
+  }
+}
